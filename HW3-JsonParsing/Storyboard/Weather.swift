e93c26e01616986515f9c8070bb43e94d274b2cd
Pavel <rezusx@mail.ru>
2024-05-15 12:22:58 +0300

    fix

--- HW3-JsonParsing/Storyboard/Weather.swift
+++ HW3-JsonParsing/Storyboard/Weather.swift
@@ -8,33 +8,33 @@
 import Foundation
 
 struct WeatherData: Decodable {
-    struct WeatherInfo: Decodable {
-        let main: String
-        let description: String
-    }
-    
-    struct MainInfo: Decodable {
-        let temp: Double
-        let pressure: Int
-        let humidity: Int
-    }
-    
-    struct WindInfo: Decodable {
-        let speed: Double
-        let deg: Int
-        let gust: Double
-    }
-
-    struct SysInfo: Decodable {
-        let country: String
-        let sunrise: Int
-        let sunset: Int
-    }
-    
     let weather: [WeatherInfo]
     let base: String
     let main: MainInfo
     let wind: WindInfo
     let sys: SysInfo
     let name: String
+}
+
+struct WeatherInfo: Decodable {
+    let main: String
+    let description: String
+}
+
+struct MainInfo: Decodable {
+    let temp: Double
+    let pressure: Int
+    let humidity: Int
+}
+
+struct WindInfo: Decodable {
+    let speed: Double
+    let deg: Int
+    let gust: Double
+}
+
+struct SysInfo: Decodable {
+    let country: String
+    let sunrise: Int
+    let sunset: Int
 }
